051274b334ee79bdc5dc087adda8421478a3b5e5
Steve Kondik <steve@cyngn.com>
2015-09-20 04:20:21 -0700

    input: Adjust priority
     * Bring into the desired group to get the best result.
    
    Change-Id: I3bd031074cd7006994736b4c22d0294b6012f662
    
    Conflicts:
    	services/inputflinger/InputManager.cpp
    	services/surfaceflinger/DispSync.cpp
    	services/surfaceflinger/EventThread.cpp
    	services/surfaceflinger/SurfaceFlinger.cpp

--- services/surfaceflinger/EventThread.cpp
+++ services/surfaceflinger/EventThread.cpp
@@ -92,7 +92,7 @@ void EventThread::sendVsyncHintOnLocked() {
 }
 
 void EventThread::onFirstRef() {
-    run("EventThread", PRIORITY_REALTIME);
+    run("EventThread", PRIORITY_URGENT_DISPLAY + PRIORITY_MORE_FAVORABLE);
     android_set_rt_ioprio(getTid(), 1);
 }
 
